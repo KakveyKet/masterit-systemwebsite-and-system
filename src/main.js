08b3a1b05cb56e8eab02b9cda43389ebf2c67c06
KakveyKet <kakveyket408@gmail.com>
2024-03-04 13:18:18 +0700

    checking some style

--- src/main.js
+++ src/main.js
@@ -2,4 +2,5 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import '../src/css/index.css'
-createApp(App).use(router).mount('#app')
+import { MotionPlugin } from '@vueuse/motion'
+createApp(App).use(router).use(MotionPlugin).mount('#app')
--- src/views/SystemDetails.vue
+++ src/views/SystemDetails.vue
@@ -8,12 +8,16 @@
         class="w-full md:w-full xl:w-[1420px] lg:w-[1420px] mx-auto space-y-4"
       >
         <div
+          v-motion-slide-left
           class="lg:mt-52 xl:mt-52 md:mt-32 mt-20 text-start lg:p-0 xl:p-0 p-3 md:p-3"
         >
           <h1 class="font-NotoSansKhmer text-primery1">Product 1</h1>
         </div>
 
-        <div class="w-full max-w-[1420px] py-4 lg:p-0 xl:p-0 p-3 md:p-3">
+        <div
+          v-motion-slide-left
+          class="w-full max-w-[1420px] py-4 lg:p-0 xl:p-0 p-3 md:p-3"
+        >
           <p class="text-textbody font-semibold text-body">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Lectus
@@ -36,8 +40,10 @@
           class="w-full max-w-[1420px] py-4 lg:p-0 xl:p-0 p-3 md:p-3 space-y-4"
         >
           <!-- details1 -->
+
           <div
-            class="w-full lg:flex xl:flex md:flex md:flex-col-reverse flex flex-col-reverse items-start justify-center space-y-4 gap-12 py-3"
+            v-motion-slide-left
+            class="w-full lg:flex lg:flex-row xl:flex md:flex md:flex-col-reverse flex flex-col-reverse items-start justify-center space-y-4 gap-12 py-3"
           >
             <div class="lg:w-1/2 xl:w-full md:w-full w-full">
               <h2
@@ -66,6 +72,7 @@
 
           <!-- details2 -->
           <div
+            v-motion-slide-left
             class="w-full lg:flex xl:flex md:block block items-start justify-center space-y-4 gap-12 py-3"
           >
             <div class="lg:w-1/2 xl:w-full md:w-full w-full">
@@ -93,7 +100,8 @@
           </div>
           <!-- details3 -->
           <div
-            class="w-full lg:flex xl:flex md:flex md:flex-col-reverse flex flex-col-reverse items-start justify-center space-y-4 gap-12 py-3"
+            v-motion-slide-left
+            class="w-full lg:flex lg:flex-row xl:flex md:flex md:flex-col-reverse flex flex-col-reverse items-start justify-center space-y-4 gap-12 py-3"
           >
             <div class="lg:w-1/2 xl:w-full md:w-full w-full">
               <h2
@@ -122,6 +130,67 @@
         </div>
       </div>
     </div>
+
+    <div>
+      <div
+        v-for="product in data"
+        :key="product"
+        class="w-full md:w-full xl:w-[1420px] lg:w-[1420px] mx-auto space-y-4"
+      >
+        <div
+          v-motion-slide-left
+          class="lg:mt-52 xl:mt-52 md:mt-32 mt-20 text-start lg:p-0 xl:p-0 p-3 md:p-3"
+        >
+          <h1 class="font-NotoSansKhmer text-primery1">
+            {{ product.productname }}
+          </h1>
+        </div>
+
+        <div
+          v-motion-slide-left
+          class="w-full max-w-[1420px] py-4 lg:p-0 xl:p-0 p-3 md:p-3"
+        >
+          <p class="text-textbody font-semibold text-body">
+            {{ product.productdes }}
+          </p>
+        </div>
+
+        <!-- taiils -->
+        <div
+          v-for="(productfeatures, index) in product.productfeature"
+          :key="index"
+          class="w-full max-w-[1420px] py-4 lg:p-0 xl:p-0 p-3 md:p-3 space-y-4"
+        >
+          <!-- details1 -->
+
+          <div
+            v-motion-slide-left
+            :class="{
+              'w-full lg:flex lg:flex-row  xl:flex xl:flex-row md:flex md:flex-col-reverse  flex flex-col-reverse items-start justify-center  space-y-4 gap-12 py-3':
+                index % 2 === 0,
+              'w-full lg:flex lg:flex-row-reverse xl:flex-row-reverse xl:flex md:flex md:flex-col-reverse flex flex-col-reverse items-start justify-center space-y-4 gap-12 py-3':
+                index % 2 !== 0,
+            }"
+          >
+            <div class="lg:w-1/2 xl:w-full md:w-full w-full">
+              <h2
+                class="text-textbody font-NotoSansKhmer text-heading3 font-semibold"
+              >
+                {{ productfeatures.feature1 }}
+              </h2>
+              <div class="flex flex-col items-center justify-center w-[90%]">
+                <p class="text-body font-NotoSansKhmer">
+                  {{ productfeatures.content }}
+                </p>
+              </div>
+            </div>
+            <div class="lg:w-1/2 xl:w-full md:w-full w-full">
+              <img class="w-full rounded-[6px]" :src="productfeatures.image" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
     <div><FooterVue /></div>
   </div>
 </template>
@@ -129,13 +198,31 @@
 <script>
 import FooterVue from "./Footer.vue";
 import NavbarVue from "./Navbar.vue";
-import { ref } from "vue";
+import { ref, onMounted, onUnmounted } from "vue";
 export default {
   components: {
     FooterVue,
     NavbarVue,
   },
   setup() {
+    const show = ref(false);
+    const windowWidth = ref(window.innerWidth);
+
+    const handleScroll = () => {
+      if (window.scrollY > 200) {
+        show.value = true;
+      } else {
+        show.value = false;
+      }
+    };
+
+    onMounted(() => {
+      window.addEventListener("scroll", handleScroll);
+    });
+
+    onUnmounted(() => {
+      window.removeEventListener("scroll", handleScroll);
+    });
     const data = ref([
       {
         productname: "Pos System",
@@ -164,10 +251,24 @@ export default {
             image:
               "https://i.pinimg.com/736x/1d/bb/82/1dbb82048e767a2a70d404ba8e8d9498.jpg",
           },
+          {
+            feature1: "feature4",
+            content:
+              "A custom design website, on the other hand, allows you to establish and express your brand through tailored page elements that align with your objectives. Custom web design cost is relatively higher than website templates, but they offer numerous benefits that give you a competitive edge. A custom WordPress website design is more search engine-friendly, customer-centric, unique and scalable than built-in templates.By choosing custom website design packages, you build a website around your specific customer journey and brand messaging.",
+            image:
+              "https://i.pinimg.com/736x/1d/bb/82/1dbb82048e767a2a70d404ba8e8d9498.jpg",
+          },
+          {
+            feature1: "feature5",
+            content:
+              "A custom design website, on the other hand, allows you to establish and express your brand through tailored page elements that align with your objectives. Custom web design cost is relatively higher than website templates, but they offer numerous benefits that give you a competitive edge. A custom WordPress website design is more search engine-friendly, customer-centric, unique and scalable than built-in templates.By choosing custom website design packages, you build a website around your specific customer journey and brand messaging.",
+            image:
+              "https://i.pinimg.com/736x/1d/bb/82/1dbb82048e767a2a70d404ba8e8d9498.jpg",
+          },
         ],
       },
     ]);
-    return { data };
+    return { data, windowWidth, show };
   },
 };
 </script>
